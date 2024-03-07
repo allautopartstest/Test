e3e3170ee046ab4302c003d15a462a53846c2b90
allautopartstest <162498352+allautopartstest@users.noreply.github.com>
2024-03-07 11:52:29 -0600

    lookup.php

--- lookup.php
+++ lookup.php
@@ -5,8 +5,8 @@ $validPostalCodes = array("A1A 1A1", "B2B 2B2", "C3C 3C3");
 // Get the entered postal code from the form
 $postalCode = $_POST['postalCode'];
 
-// Log the received postal code to a file for debugging
-file_put_contents('debug.log', "Received postal code: $postalCode\n", FILE_APPEND);
+// Print out the received postal code for debugging
+echo "Received postal code: $postalCode<br>";
 
 // Check if the entered postal code exists in the list
 if (in_array($postalCode, $validPostalCodes)) {
